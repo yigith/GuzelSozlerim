b77a444b781f64bdb107ae3538833dd25e94a703
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-04-21 13:13:39 +0300

    beğeniler

--- GuzelSozlerim/Controllers/HomeController.cs
+++ GuzelSozlerim/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using GuzelSozlerim.Data;
+using GuzelSozlerim.Extensions;
 using GuzelSozlerim.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace GuzelSozlerim.Controllers
@@ -23,7 +27,39 @@ namespace GuzelSozlerim.Controllers
 
         public IActionResult Index()
         {
-            return View(_db.GuzelSozler.ToList());
+            return View(_db.GuzelSozler.Include(x => x.Begenenler).ToList());
+        }
+
+        [Authorize] // Sadece üyeler kullanabilir
+        [HttpPost]
+        public IActionResult BegeniDurumunuGuncelle(int id, bool begenildiMi)
+        {
+            try
+            {
+                string userId = User.GetUserId();
+                var begeni = new KullaniciSoz() { GuzelSozId = id, KullaniciId = userId };
+
+                if (begenildiMi)
+                {
+                    if (!_db.KullaniciSozler.Contains(begeni))
+                    {
+                        _db.KullaniciSozler.Add(begeni);
+                    }
+                }
+                else
+                {
+                    if (_db.KullaniciSozler.Contains(begeni))
+                    {
+                        _db.KullaniciSozler.Remove(begeni);
+                    }
+                }
+                _db.SaveChanges();
+                return new EmptyResult();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
         }
 
         public IActionResult Privacy()
---
+++ GuzelSozlerim/Extensions/IdentityHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GuzelSozlerim.Extensions
+{
+    public static class IdentityHelpers
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
